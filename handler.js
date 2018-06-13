5d27ac698ec6110b55fc447cea24a32e89c2306e
Hugh Wimberly <hugh.wimberly@gmail.com>
2018-06-12 23:05:00 -0700

    Working with TODO list items

--- handler.js
+++ handler.js
@@ -1,16 +1,108 @@
-'use strict';
+"use strict";
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+const uuid = require("uuid");
+const AWS = require("aws-sdk"); // eslint-disable-line import/no-extraneous-dependencies
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.create = (event, context, callback) => {
+  const timestamp = new Date().getTime();
+  const data = JSON.parse(event.body);
+  if (typeof data.text !== "string") {
+    console.error("Validation Failed");
+    callback(null, {
+      statusCode: 400,
+      headers: { "Content-Type": "text/plain" },
+      body: "Couldn't create the todo item."
+    });
+    return;
+  }
+
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Item: {
+      id: uuid.v1(),
+      text: data.text,
+      checked: false,
+      createdAt: timestamp,
+      updatedAt: timestamp
+    }
   };
 
-  callback(null, response);
+  // write the todo to the database
+  dynamoDb.put(params, error => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { "Content-Type": "text/plain" },
+        body: "Couldn't create the todo item."
+      });
+      return;
+    }
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(params.Item)
+    };
+    callback(null, response);
+  });
+};
+
+module.exports.get = (event, context, callback) => {
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: {
+      id: event.pathParameters.id
+    }
+  };
+
+  // fetch todo from the database
+  dynamoDb.get(params, (error, result) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { "Content-Type": "text/plain" },
+        body: "Couldn't fetch the todo item."
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Item)
+    };
+    callback(null, response);
+  });
+};
+
+module.exports.list = (event, context, callback) => {
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE
+  };
+  // fetch all todos from the database
+  dynamoDb.scan(params, (error, result) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { "Content-Type": "text/plain" },
+        body: "Couldn't fetch the todos."
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Items)
+    };
+    callback(null, response);
+  });
 };
